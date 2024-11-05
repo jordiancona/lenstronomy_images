7c232c255e900336992bdf3efbd8f07ea0cef684
jordiancona <anconafloresj@gmail.com>
2024-11-05 11:57:27 -0600

    5 de nov

--- lens_class.py
+++ lens_class.py
@@ -1,1 +1,9 @@
-import pandas as pd
+import lenstronomy as ln
+import numpy as np
+
+class lens:
+    def __init__(self, total_images = 1000, model = 'SIE') -> None:
+        self.total_images = total_images
+        self.model = model
+    
+
